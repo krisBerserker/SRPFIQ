f8e41178ea88b0d5a93c6f5d2416ee34854ca813
lavitesse2 <2293518@cegeplimoilou.ca>
2025-06-25 13:36:58 -0400

    first commit

---
+++ Migrations/20250625152018_secondcreated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication_SRPFIQ.Migrations
+{
+    /// <inheritdoc />
+    public partial class secondcreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
