9ede868ddbcca152e51974a7b6a5db0188f3eeff
lavitesse2 <2293518@cegeplimoilou.ca>
2025-06-25 13:36:58 -0400

    first commit

--- Migrations/20250625163007_modifyquestionnaire.Designer.cs
+++ Migrations/20250625163007_modifyquestionnaire.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebApplication_SRPFIQ.Data;
 
@@ -11,9 +12,11 @@ using WebApplication_SRPFIQ.Data;
 namespace WebApplication_SRPFIQ.Migrations
 {
     [DbContext(typeof(SRPFIQDbContext))]
-    partial class SRPFIQDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250625163007_modifyquestionnaire")]
+    partial class modifyquestionnaire
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
