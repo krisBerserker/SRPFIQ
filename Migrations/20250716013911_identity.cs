b186412a2473d4ec2d64890d64b99b8521807687
Christian SOPKAM <sopkamchristian@gmail.com>
2025-07-16 09:56:57 -0400

    Debuts des pages

---
+++ Migrations/20250716013911_identity.cs
@@ -0,0 +1,375 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApplication_SRPFIQ.Migrations
+{
+    /// <inheritdoc />
+    public partial class identity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_Users_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_Users_UserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_Users_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_Users_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MedicalNotes_Users_IdUser",
+                table: "MedicalNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meetings_Users_IdUser",
+                table: "Meetings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionnaireAnswers_Users_IdUser",
+                table: "QuestionnaireAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RequestNotes_Users_IdUser",
+                table: "RequestNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_IdUserClosedBy",
+                table: "Requests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAssignedRequests_Users_IdUser",
+                table: "UserAssignedRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserPermissions_Users_IdUser",
+                table: "UserPermissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "Users",
+                newName: "ID");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ID",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_Users_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_Users_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_Users_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_Users_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MedicalNotes_Users_IdUser",
+                table: "MedicalNotes",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meetings_Users_IdUser",
+                table: "Meetings",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionnaireAnswers_Users_IdUser",
+                table: "QuestionnaireAnswers",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RequestNotes_Users_IdUser",
+                table: "RequestNotes",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_IdUserClosedBy",
+                table: "Requests",
+                column: "IdUserClosedBy",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAssignedRequests_Users_IdUser",
+                table: "UserAssignedRequests",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserPermissions_Users_IdUser",
+                table: "UserPermissions",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserClaims_Users_UserId",
+                table: "AspNetUserClaims");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserLogins_Users_UserId",
+                table: "AspNetUserLogins");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserRoles_Users_UserId",
+                table: "AspNetUserRoles");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUserTokens_Users_UserId",
+                table: "AspNetUserTokens");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_MedicalNotes_Users_IdUser",
+                table: "MedicalNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Meetings_Users_IdUser",
+                table: "Meetings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_QuestionnaireAnswers_Users_IdUser",
+                table: "QuestionnaireAnswers");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_RequestNotes_Users_IdUser",
+                table: "RequestNotes");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Requests_Users_IdUserClosedBy",
+                table: "Requests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserAssignedRequests_Users_IdUser",
+                table: "UserAssignedRequests");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_UserPermissions_Users_IdUser",
+                table: "UserPermissions");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "ID",
+                table: "Users",
+                newName: "Id");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "UserName",
+                table: "Users",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(256)",
+                oldMaxLength: 256);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserClaims_Users_UserId",
+                table: "AspNetUserClaims",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserLogins_Users_UserId",
+                table: "AspNetUserLogins",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserRoles_Users_UserId",
+                table: "AspNetUserRoles",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUserTokens_Users_UserId",
+                table: "AspNetUserTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_MedicalNotes_Users_IdUser",
+                table: "MedicalNotes",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Meetings_Users_IdUser",
+                table: "Meetings",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_QuestionnaireAnswers_Users_IdUser",
+                table: "QuestionnaireAnswers",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_RequestNotes_Users_IdUser",
+                table: "RequestNotes",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Requests_Users_IdUserClosedBy",
+                table: "Requests",
+                column: "IdUserClosedBy",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserAssignedRequests_Users_IdUser",
+                table: "UserAssignedRequests",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_UserPermissions_Users_IdUser",
+                table: "UserPermissions",
+                column: "IdUser",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
